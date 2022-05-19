5f7616f6d11e09e7ccb9b979779c134abf012d98
James Oresti <j.oresti@gmail.com>
2022-05-19 15:44:27 -0400

    ImageFilter added

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,23 +14,27 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // Returns filtered image
+  // Note issues 
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const query = req.query;
+    if (!query || !query['image_url']) {
+      return res.status(400).send("try GET /filteredimage?image_url={{}}");
+    }
 
-  /**************************************************************************** */
+    const imageUrl = query['image_url'];
 
-  //! END @TODO1
+    try {
+      const validUrl = new URL(imageUrl);
+    } catch(err) {
+      return res.status(400).send("Provided image_url is not valid http");
+    }
+
+    const result = await filterImageFromURL(imageUrl);
+
+    return res.status(200).sendFile(result);
+  });
+
   
   // Root Endpoint
   // Displays a simple message to the user
